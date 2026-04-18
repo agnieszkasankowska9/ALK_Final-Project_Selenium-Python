bfdd74afe98e6150b3c506206fdd6ee64bb6cfe0
agnieszkasankowska9 <agnieszkasankowska9@gmail.com>
2026-04-18 13:00:41 +0000

    first_address_test testZipPostalCode added

--- tests/first_address_test.py
+++ tests/first_address_test.py
@@ -59,6 +59,7 @@ class FirstAddressTest(BaseTest):
         self.assertIn(expectederrors, visible_errors)
 
 
+    @unittest.skip("Temporary skipping")
     @data(*test_data.first_address_data.get_csv_data("test_data/first_address_data_all.csv"))
     @unpack
     def testNoState(self, testcaseid, firstname, lastname, company, address, address2, city, state, postalcode, country, homephone, mobilephone, additionalinfo, addresstitle):
@@ -77,3 +78,25 @@ class FirstAddressTest(BaseTest):
         expected_errors = ["This country requires you to chose a State."]
         self.assertCountEqual(expected_errors, visible_errors)
         sleep(1)
+
+    # @unittest.skip("Temporary skipping")
+    @data(*test_data.first_address_data.get_csv_data("test_data/first_address_zip_postal_code_negative.csv"))
+    @unpack
+    def testZipPostalCode(self, testcaseid, firstname, lastname, company, address, address2, city, state, postalcode, country,
+                    homephone, mobilephone, additionalinfo, addresstitle):
+        self.assertEqual(self.data.firstname, self.my_address_page.get_entered_first_name())
+        self.assertEqual(self.data.lastname, self.my_address_page.get_entered_last_name())
+        self.my_address_page.enter_address(address)
+        self.my_address_page.enter_city(city)
+        self.my_address_page.select_state(state)
+        self.my_address_page.enter_zip_post_code(postalcode)
+        self.my_address_page.select_country(country)
+        self.my_address_page.enter_home_phone(homephone)
+        self.my_address_page.click_save_button()
+        expected_number_of_errors_message = "There is 1 error"
+        actual_number_of_errors_message = self.my_address_page.get_number_of_errors_message()
+        self.assertEqual(expected_number_of_errors_message, actual_number_of_errors_message)
+        visible_errors = self.my_address_page.get_visible_errors()
+        expected_errors = ["The Zip/Postal code you've entered is invalid. It must follow this format: 00000"]
+        self.assertCountEqual(expected_errors, visible_errors)
+        sleep(3)
